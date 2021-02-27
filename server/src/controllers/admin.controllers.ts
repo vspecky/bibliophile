8e283d1319d35448f72b38fbc5d828e33aef7819
vspecky <thelonenerd9913@gmail.com>
2021-02-27 19:33:01 +0530

    Modified the admin.deleteTopicOrPost controller to add a Mod Action to the database

--- server/src/controllers/admin.controllers.ts
+++ server/src/controllers/admin.controllers.ts
@@ -1,21 +1,39 @@
 import { Request, Response } from 'express';
 import * as globals from '../globals';
+import ModAction from '../models/modaction.model';
 import Topic from '../models/topic.model';
 
 export const deleteTopicOrPost = async (req: Request, res: Response) => {
     const topicID = req.params.id;
     const postID = req.query.postid;
+    const reason = req.query.reason;
 
     if (!postID) {
         Topic.findByIdAndDelete(topicID)
-            .exec(async (err, _) => {
+            .exec(async (err, doc) => {
                 if (err) {
                     console.error(err);
                     res.status(500).json(globals.INTERNAL_SERVER_ERROR);
                     return;
                 }
 
-                res.sendStatus(200);
+                if (!doc) {
+                    res.sendStatus(404);
+                    return;
+                }
+
+                const modAction = new ModAction({
+                    actionType: "topic",
+                    document: doc,
+                    reason,
+                });
+
+                modAction.save()
+                    .then(async final => res.status(204).json(final))
+                    .catch(async err => {
+                        console.error(err);
+                        res.status(500).json(globals.INTERNAL_SERVER_ERROR);
+                    });
             });
 
         return;
@@ -28,13 +46,48 @@ export const deleteTopicOrPost = async (req: Request, res: Response) => {
             }
         }
     })
-    .exec(async (err, _) => {
+    .exec(async (err, doc) => {
         if (err) {
             console.error(err);
             res.status(500).json(globals.INTERNAL_SERVER_ERROR);
             return;
         }
 
-        res.sendStatus(200);
+        if (!doc) {
+            res.sendStatus(404);
+            return;
+        }
+
+        if (doc.posts.length > 0 && doc.posts[0]._id.toString() === postID) {
+            const modAction = new ModAction({
+                actionType: "topic",
+                document: doc,
+                reason,
+            });
+
+            modAction.save()
+                .then(async final => res.status(204).json(final))
+                .catch(async err => {
+                    console.error(err);
+                    res.status(500).json(globals.INTERNAL_SERVER_ERROR);
+                });
+
+            return;
+        }
+
+        const post = doc.posts.find(p => p._id.toString() === postID);
+
+        const modAction = new ModAction({
+            actionType: "post",
+            document: post,
+            reason,
+        });
+
+        modAction.save()
+            .then(async final => res.status(204).json(final))
+            .catch(async err => {
+                console.error(err);
+                res.status(500).json(globals.INTERNAL_SERVER_ERROR);
+            });
     });
 };
